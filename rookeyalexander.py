2046f01ca3280894d5f39aeabcbc01af9bd2d80f
Alexander Rookey <rookierookey@gmail.com>
2015-03-23 14:18:21 -0400

    working

--- rookeyalexander.py
+++ rookeyalexander.py
@@ -1,20 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 import pymongo
 
 app = Flask(__name__)
 
 MONGODB_URI = 'mongodb://heroku_app35160490:n52q2opnmfvpluv4rvvfthi2p4@ds035240.mongolab.com:35240/heroku_app35160490'
+RANGE = 1
 
 client = pymongo.MongoClient(MONGODB_URI)
 db = client.get_default_database()
 
-@app.route('/')
-def home():
-  return render_template('home.html')
+@app.route('/get_image', methods=['GET'])
+def get_image():
+  latitude = request.args.get('latitude')
+  longitude = request.args.get('longitude')
+  if(latitude == None or longitude == None):
+    return jsonify({})
+  latitude = float(latitude)
+  longitude = float(longitude)
+  lat_min = latitude - RANGE
+  lat_max = latitude + RANGE
+  long_min = longitude - RANGE
+  long_max = longitude + RANGE
+  image_doc = db.images.find_one({'latitude': { '$gt': lat_min, '$lt': lat_max },'longitude': { '$gt': long_min, '$lt': long_max }})
+  if(image_doc == None):
+    return jsonify({})
+  db.images.remove({'_id':image_doc.get('_id')})
+  return jsonify({'image': image_doc.get('image')})
 
-@app.route('/contact')
-def contact():
-  return render_template('contact.html')
+@app.route('/store_image', methods=['POST'])
+def store_image():
+  latitude = request.form['latitude']
+  longitude = request.form['longitude']
+  image = request.form['image']
+  latitude = float(latitude)
+  longitude = float(longitude)
+  post = {'latitude': latitude,
+          'longitude': longitude,
+          'image': image }
+  db.images.insert(post)
+  return 'post successful'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
