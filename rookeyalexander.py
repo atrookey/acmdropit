93f18f64d2bd4144d5e1b1d4d250fbf847bbab71
Alexander Rookey <rookierookey@gmail.com>
2015-03-23 14:35:32 -0400

    set range

--- rookeyalexander.py
+++ rookeyalexander.py
@@ -4,7 +4,7 @@ import pymongo
 app = Flask(__name__)
 
 MONGODB_URI = 'mongodb://heroku_app35160490:n52q2opnmfvpluv4rvvfthi2p4@ds035240.mongolab.com:35240/heroku_app35160490'
-RANGE = 1
+RANGE = 0.0000015
 
 client = pymongo.MongoClient(MONGODB_URI)
 db = client.get_default_database()
